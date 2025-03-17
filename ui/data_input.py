50d7ca2ab6c2223aa25559be9dc7512b6e350184
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-03-17 14:39:35 +0500

    Update data_input.py

--- ui/data_input.py
+++ ui/data_input.py
@@ -56,15 +56,15 @@ class DataInput:
         """
         Экран для ввода параметров и выбора сетки скважин.
         """
-        st.header("Ввод параметров и выбор сеткиТест")
+        st.header("Ввод параметров и выбор сетки")
     
-        # # Проверяем наличие имени блока
-        # block_name = st.session_state.get("block_name")
+        # Проверяем наличие имени блока
+        block_name = st.session_state.get("block_name")
     
-        # if block_name:
-        #     st.info(f"Импортированный блок: **{block_name}**")
-        # else:
-        #     st.subheader.warning("Блок не импортирован. Импортируйте блок на вкладке 'Импорт данных блока'.")
+        if block_name:
+            st.info(f"Импортированный блок: **{block_name}**")
+        else:
+            st.subheader.warning("Блок не импортирован. Импортируйте блок на вкладке 'Импорт данных блока'.")
 
         st.subheader("Настройка параметров блока")
         self.input_form.render_parameters_section()  # Отображаем параметры
