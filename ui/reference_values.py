beaf3c9fb655c92aef653d349fc895e22d664586
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-03-19 13:52:38 +0500

    Update reference_values.py

--- ui/reference_values.py
+++ ui/reference_values.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from utils.session_state_manager import SessionStateManager
 from utils.logs_manager import LogsManager
+from modules.reference_parameters import ReferenceParameters
 
 class RefValues:
     def __init__(self, session_manager: SessionStateManager, logs_manager: LogsManager):
@@ -25,3 +26,7 @@ class RefValues:
             st.warning("Блок не импортирован. Импортируйте блок на вкладке 'Импорт данных блока'.")
         else:
             st.info(f"Импортированный блок: **{block_name}**")
+
+        # ✅ Создаем объект ReferenceParameters и рендерим параметры
+        ref_params = ReferenceParameters(self.session_manager, self.logs_manager)
+        ref_params.render_refparameters_section()
