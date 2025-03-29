cdd3db2c80860b1d72979b769e793bddf9d76e35
Niyaz Akhmetov <niazakhmetov@gmail.com>
2025-03-29 23:40:56 +0500

    Update results_summary.py

--- ui/results_summary.py
+++ ui/results_summary.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from modules.fragmentation_calculator import FragmentationCalculator
-from modules.psd_calculator import PSDCalculator
-from modules.results_display import ResultsDisplay
+from modules.calculations import Calculations
+# from modules.psd_calculator import PSDCalculator
+# from modules.results_display import ResultsDisplay
 from utils.session_state_manager import SessionStateManager
 from utils.logs_manager import LogsManager
 
@@ -12,9 +12,10 @@ class ResultsSummary:
     def __init__(self, session_manager: SessionStateManager, logs_manager: LogsManager):
         self.session_manager = session_manager
         self.logs_manager = logs_manager
-        self.calculator = FragmentationCalculator(session_manager)
-        self.psd_calculator = PSDCalculator(session_manager)
-        self.results_display = ResultsDisplay()
+        self.calculator = Calculations(session_manager, logs_manager)
+        # self.psd_calculator = PSDCalculator(session_manager)
+        # self.results_display = ResultsDisplay()
+
 
     def show_results_summary(self):
         """
@@ -30,73 +31,10 @@ class ResultsSummary:
         else:
             st.info(f"Импортированный блок: **{block_name}**")
 
-
-        # Кнопка для запуска расчётов
-        if st.button("Запустить расчёты"):
+        # Кнопка для запуска расчётов БВР
+        if st.button("Запустить расчеты БВР"):
             with st.spinner("Выполняются расчёты..."):
-                self.calculator.run_calculations()  # Расчёт параметров БВР
-    #             self.psd_calculator.run_calculations()  # Формирование PSD-таблицы
-
-
-    #     # Блок визуализации
-    #     st.subheader("Визуализация результатов")
-
-    #     col1, col2 = st.columns(2)
-
-    #     with col1:
-    #         if st.button("Показать таблицу PSD"):
-    #             self.display_psd_table()
-
-    #     with col2:
-    #         if st.button("Показать кумулятивную кривую"):
-    #             self.display_cumulative_curve()
-
-    #     # Блок очистки данных
-    #     st.subheader("Очистка визуализации")
-
-    #     col3, col4 = st.columns(2)
-
-    #     with col3:
-    #         if st.button("Очистить таблицу"):
-    #             self.session_manager.set_state("psd_table", None)
-    #             st.warning("Таблица PSD очищена.")
-
-    #     with col4:
-    #         if st.button("Очистить график"):
-    #             self.session_manager.set_state("cumulative_curve", None)
-    #             st.warning("График кумулятивной кривой очищен.")
-
-    # def display_psd_table(self):
-    #     """
-    #     Отображение итоговой PSD-таблицы с x_values, P(x) эталонные и P(x) расчётные.
-    #     """
-    #     df_psd = st.session_state.get("psd_table")
-
-    #     if df_psd is None or df_psd.empty:
-    #         st.warning("PSD-таблица отсутствует.")
-    #         return
-
-    #     st.subheader("Итоговая таблица PSD")
-    #     st.dataframe(df_psd)
-
-    # def display_cumulative_curve(self):
-    #     """
-    #     Визуализация кумулятивной кривой распределения фрагментов.
-    #     """
-    #     df_psd = st.session_state.get("psd_table")
-
-    #     if df_psd is None or df_psd.empty:
-    #         st.warning("Нет данных для построения графика.")
-    #         return
-
-    #     import plotly.express as px
-
-    #     fig = px.line(
-    #         df_psd,
-    #         x="Размер фрагмента (x), мм",
-    #         y=["Эталонные P(x), %", "P(x) рассчитанные, %"],
-    #         title="Кумулятивная кривая распределения фрагментов",
-    #         labels={"value": "Кумулятивное распределение (%)", "variable": "Тип"},
-    #         markers=True
-    #     )
-    #     st.plotly_chart(fig)
+                try:
+                    from modules.calculations import Calculations  # Импорт объединённого модуля
+                    calc = Calculations(self.session_manager, self.logs_manager)
+                    calc.run_all_calculations()  # Запуск всех расчётов
